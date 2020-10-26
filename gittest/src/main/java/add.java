2973509c6f76ba4bb48806223c5070f038a02aed
long12360 <154275017@qq.com>
2020-10-26 10:27:52 +0800

    创建项目

---
+++ gittest/src/main/java/add.java
@@ -0,0 +1,11 @@
+/**
+ * @Author long_12360
+ * @Description: test
+ * @Date Created in 2020/10/26 9:56
+ * @Verson 1.0
+ */
+public class add {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
